644749efa443ac742acb28115425150a2f791bb9
DevFajardo <devfajardocadena@gmail.com>
2024-09-13 22:07:04 -0500

    fixing errors

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-export default function Home() {
-  return <h1>hola</h1>;
-=======
 'use client'
 import useLoginState from './LoginUtils/useLoginState.js';
 
@@ -39,5 +35,4 @@ export default function Home() {
       </div>
     </div>
   );
->>>>>>> d20369314f4e0097a5315d3a465e012b5b2c92e0
 }
