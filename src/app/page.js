85df08f99ad9d84dea0b5f9823f3134904cf6d9d
AntonyOspino <tonny1998_thony@hotmail.com>
2024-09-13 22:31:33 -0500

    actualizacion de Card + Css

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
-export default function Home() {
-  return <h1>hola</h1>;
-=======
-'use client'
-import useLoginState from './LoginUtils/useLoginState.js';
+"use client";
+import useLoginState from "./LoginUtils/useLoginState.js";
 
 export default function Home() {
   const { data, handleData, handleSubmit } = useLoginState();
   return (
-    <div className={'login'}>
-      <div className={'login_container'}>
+    <div className={"login"}>
+      <div className={"login_container"}>
         <form onSubmit={handleSubmit}>
-          <h2 id='loginsesion'>Iniciar Sesión</h2>
-          {data && data.error && <p className={'error'}>{data.error}</p>}
-          <div className={'form-group'}>
+          <h2 id="loginsesion">Iniciar Sesión</h2>
+          {data && data.error && <p className={"error"}>{data.error}</p>}
+          <div className={"form-group"}>
             <input
               type="text"
               id="username"
@@ -23,7 +19,7 @@ export default function Home() {
               placeholder="Usuario"
             />
           </div>
-          <div className={'form-group'}>
+          <div className={"form-group"}>
             <input
               type="password"
               id="password"
@@ -34,10 +30,11 @@ export default function Home() {
             />
           </div>
 
-          <button type="submit" className={'login-button'}>Iniciar Sesión</button>
+          <button type="submit" className={"login-button"}>
+            Iniciar Sesión
+          </button>
         </form>
       </div>
     </div>
   );
->>>>>>> d20369314f4e0097a5315d3a465e012b5b2c92e0
 }
