f78273ae3477d1dd6bc7cb4d10e1557e69195944
DevFajardo <devfajardocadena@gmail.com>
2024-09-27 14:47:13 -0500

    edit call api

--- src/app/pages/dashboard/page.jsx
+++ src/app/pages/dashboard/page.jsx
@@ -1,12 +1,14 @@
-import { habitacionesGet } from "@/utils/peticiones";
+import { peticionGet } from "@/utils/peticiones";
 import Cards from "./components/cardMain";
 
 export default async function Recepcionista() {
   //llamamos la funcion handler importada de actions para traer a este componente la lista de las habitaciones
-  const habitaciones = await habitacionesGet();
+  const habitaciones = await peticionGet(
+    "http://localhost:3000/api/habitaciones"
+  );
   return (
     <>
-    <Cards habitaciones={habitaciones} />
+      <Cards habitaciones={habitaciones} />
     </>
   );
 }
