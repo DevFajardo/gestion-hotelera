9d9bc0d9e86a0c57729574bdd9e9fcd3b3eafd85
DevFajardo <devfajardocadena@gmail.com>
2024-09-27 21:08:02 -0500

    add method delete adn create from endpoint empleado

--- src/app/api/empleados/route.js
+++ src/app/api/empleados/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { sql } from "@/app/api/lib/bd";
+import { messages } from "@/utils/messages";
 
 export async function GET() {
   try {
@@ -8,5 +9,59 @@ export async function GET() {
     return NextResponse.json(response);
   } catch (error) {
     return NextResponse.json({ error });
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    const body = await req.json();
+    const { identificacion } = body;
+    if (identificacion) {
+      const response =
+        await sql`DELETE from empleado where n_identificacion = ${identificacion}`;
+      if (response) {
+        return NextResponse.json({ message: messages.succes.deleteSucces });
+      } else {
+        return NextResponse.json({
+          message: messages.error.notFoundCredentials,
+        });
+      }
+    }
+  } catch (error) {
+    return NextResponse.json({ message: messages.error.defaultError });
+  }
+}
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    if (body) {
+      const {
+        created_at,
+        tipo_identificacion,
+        n_identificacion,
+        nombre,
+        apellido,
+        telefono,
+        direccion,
+        usuario,
+        contraseña,
+        id_rol,
+        estado,
+      } = body;
+      const review =
+        await sql`SELECT n_identificacion from empleado where n_identificacion = ${n_identificacion}`;
+      if (review[0] == null) {
+        const create =
+          await sql`INSERT INTO "public"."empleado" ("created_at", "tipo_identificacion", "n_identificacion", "nombre", "apellido", "telefono", "direccion", "usuario", "contraseña", "id_rol", "estado") VALUES ( ${created_at}, ${tipo_identificacion}, ${n_identificacion}, ${nombre}, ${apellido}, ${telefono}, ${direccion}, ${usuario}, ${contraseña}, ${id_rol}, ${estado})`;
+        return NextResponse.json({ message: messages.succes.createSucces });
+      } else {
+        return NextResponse.json({
+          message: messages.error.duplicateCredentials,
+        });
+      }
+    }
+  } catch (error) {
+    return NextResponse.json({ message: messages.error.defaultError });
   }
 }
