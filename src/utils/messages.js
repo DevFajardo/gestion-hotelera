872e869bdd99e3d2a4cb48f5c93fe7748ad5ff2a
DevFajardo <devfajardocadena@gmail.com>
2024-09-27 21:07:30 -0500

    add messages folder utils

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -6,10 +6,14 @@ export const messages = {
     defaultError: "error con la operacion",
     notFoundToken: "no existe el token",
     notFoundCookie: "no existe la cookie",
+    notFoundCredentials: "no se econtro el registro",
+    duplicateCredentials: "El registro ya esta creado"
   },
   succes: {
     sessionInit: "sesion iniciada con exito",
     logoutSucces: "session cerrada con exito",
-    getSucces: "peticion get exitosa"
+    getSucces: "peticion get exitosa",
+    deleteSucces : "registro eliminado correctamente",
+    createSucces : "registro creado exitosamente"
   },
 };
